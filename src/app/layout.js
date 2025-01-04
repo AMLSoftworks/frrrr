c12ec1ded041d22ca64726db83ee1a5ddcd4234a
AMLSoftworks <tristen30081@gmail.com>
2025-01-04 17:21:19 +0100

    Add files via upload

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,27 +17,27 @@ const poppins = Poppins({
 });
 
 export const metadata = {
-  title: "TunePulse - Your Personalized Music Experience",
+  title: "QueueIt - Your Personalized Music Experience",
   description:
-    "TunePulse - A music streaming app designed to deliver the best music experience with curated playlists, song history, and seamless playback. Created by Vaivik Shah.",
+    "QueueIt - A music streaming app designed to deliver the best music experience with curated playlists, song history, and seamless playback. Created by Sillyfade.",
   image:
     "https://res.cloudinary.com/dbr73rpz9/image/upload/v1690380865/images/logo-color_noktgr.png",
-  url: "https://tunepulse.vercel.app",
+  url: "https://QueueIt.vercel.app",
   type: "website",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
-  site_name: "TunePulse",
+  site_name: "QueueIt",
   manifest: "/manifest.json",
-  author: "Vaivik Shah",
-  "og:title": "TunePulse - Your Personalized Music Experience",
+  author: "Sillyfade",
+  "og:title": "QueueIt - Your Personalized Music Experience",
   "og:description":
-    "TunePulse - The ultimate music streaming platform created by Vaivik Shah, offering curated playlists and seamless playback.",
+    "QueueIt - The ultimate music streaming platform created by Sillyfade, offering curated playlists and seamless playback.",
   "og:image":
     "https://res.cloudinary.com/dbr73rpz9/image/upload/v1690380865/images/logo-color_noktgr.png",
-  "og:url": "https://tunepulse.vercel.app",
+  "og:url": "https://QueueIt.vercel.app",
   "og:type": "website",
-  "og:site_name": "TunePulse",
+  "og:site_name": "QueueIt",
   keywords:
-    "music streaming, playlists, audio player, songs history, TunePulse, Vaivik Shah",
+    "music streaming, playlists, audio player, songs history, QueueIt, Sillyfade",
   "theme-color": "#1DB954",
   "apple-touch-icon": [
     { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
